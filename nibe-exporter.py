f7f0a624187dbdddea2403614f3928b965cae952
Jonas Eana <jonas@eana.ro>
2023-05-30 20:53:36 +0200

    Optimize the script

--- nibe-exporter.py
+++ nibe-exporter.py
@@ -10,9 +10,7 @@ import requests
 from bs4 import BeautifulSoup
 from prometheus_client import Gauge, start_http_server
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.WARNING, format="%(message)s")
 
 
 class CustomExporter:
@@ -22,60 +20,57 @@ class CustomExporter:
         self.username = username
         self.password = password
         self.metric_dict = {}
-
+        self.logged_in = False
+        self.last_login_check_time = 0
         self.s = requests.Session()
 
-    def check_login(self, html: str) -> bool:
-        if html.find('Log out') == -1:
-            logging.info(f"Failed to log in to {self.url}")
-            logged_in = False
-        else:
-            logged_in = True
-
-        return logged_in
-
     def login(self) -> bool:
+        if self.logged_in or (time.time() - self.last_login_check_time
+                              ) < 60:  # Check interval: 60 seconds
+            return self.logged_in
+
         payload = {
             "Email": self.username,
             "Password": self.password,
         }
 
-        self.s.post(url + "/LogIn", data=payload)
+        response = self.s.post(self.url + "/LogIn", data=payload)
+        html = response.content.decode("utf-8")
 
+        if html.find("Log out") == -1:
+            self.logged_in = False
+        else:
+            self.logged_in = True
+
+        self.last_login_check_time = time.time(
+        )  # Update last login check time
+        return self.logged_in
+
+    def check_login(self) -> None:
         res = self.s.get(self.url)
         html = res.content.decode("utf-8")
 
-        if not self.check_login(html):
-            logging.info(f"Failed to log in to {self.url}")
-            return False
-
-        return True
+        if html.find("Log out") == -1:
+            self.logged_in = False
+        else:
+            self.logged_in = True
 
     def get_data(self) -> str:
         res = self.s.get(self.url + "/System/169626/Status/ServiceInfo")
-
         return res.content.decode("utf-8")
 
     def replace_chars(self, metric_name) -> str:
-        for ch in [" ", "-"]:
-            if ch in metric_name:
-                metric_name = (metric_name.replace(ch, "_").replace(
-                    ".", "").replace(",", ""))
-
-        return metric_name
+        return re.sub(r"[ -.,]+", "_", metric_name)
 
     def create_metrics(self):
-        logging.info("Create the metrics")
+        if not self.logged_in or (time.time() -
+                                  self.last_login_check_time) >= 60:
+            self.check_login()  # Check login status periodically
+            self.last_login_check_time = time.time(
+            )  # Update last login check time
 
         html = self.get_data()
         match = r"^[-+]?\d+[,.]?\d*"
-
-        if not self.check_login(html):
-            logging.info(f"Not logged in. Logging in to {self.url}")
-            if not self.login():
-                print("Couldn't login. Exiting.")
-                exit(1)
-
         soup = BeautifulSoup(html, features="html.parser")
 
         for span in soup.find_all("span"):
@@ -94,39 +89,41 @@ class CustomExporter:
                 else:
                     value = None
 
-                if self.metric_dict.get(metric_name) is None:
-                    self.metric_dict[metric_name] = Gauge(metric_name, "")
+                metric = self.metric_dict.get(metric_name)
+                if metric is None:
+                    metric = Gauge(metric_name, "")
+                    self.metric_dict[metric_name] = metric
 
-                if type(value) == int or type(value) == float:
-                    self.metric_dict[metric_name].set(value)
+                if isinstance(value, (int, float)):
+                    metric.set(value)
 
     def main(self):
         frequency = 15
         exporter_port = int(environ.get("EXPORTER_PORT", "9877"))
-        start_http_server(exporter_port)
-        logging.info(f"Listening on {exporter_port}")
+        start_http_server(exporter_port, addr='0.0.0.0')
+        logging.warning(f"Listening on 0.0.0.0:{exporter_port}")
 
-        logging.info(f"Logging in to {self.url}")
+        logging.warning(f"Logging in to {self.url}")
         if not self.login():
             print("Couldn't login. Exiting.")
             exit(1)
 
+        logging.warning("Script started to listen for incoming requests.")
+
         while True:
             self.create_metrics()
-            logging.info(f"Wait for {frequency} seconds")
             time.sleep(frequency)
 
 
 if __name__ == "__main__":
     url = "https://www.nibeuplink.com"
+    username = environ.get("NIBE_USERNAME")
+    password = environ.get("NIBE_PASSWORD")
 
-    if ("NIBE_USERNAME" not in environ) or ("NIBE_PASSWORD" not in environ):
-        print('You need to define the NIBE_USERNAME and NIBE_PASSWORD '
-              'environment variables.')
+    if not (username and password):
+        print("You need to define the NIBE_USERNAME and NIBE_PASSWORD "
+              "environment variables.")
         exit(1)
-
-    username = environ['NIBE_USERNAME']
-    password = environ['NIBE_PASSWORD']
 
     c = CustomExporter(url, username, password)
     c.main()
